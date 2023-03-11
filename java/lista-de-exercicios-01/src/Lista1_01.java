6bc61ca2d131fd4fa4b547e4f4aeae70ad3acb2d
Pablo Quiroga <106888394+quirogahub@users.noreply.github.com>
2023-03-10 22:08:49 -0300

    Exercício 01 da lista 01

---
+++ java/lista-de-exercicios-01/src/Lista1_01.java
@@ -0,0 +1,30 @@
+		/* Construa um algoritmo que leia (via teclado) dois números
+		 * do tipo inteiro, calcule o produto entre eles e mostre o resultado.
+		 */
+
+import java.util.Scanner;
+
+public class Lista1_01 {
+
+	public static void main(String[] args) {
+		
+		// Declaração
+		Scanner entrada = new Scanner(System.in);
+		int primeiroNumero, segundoNumero, produto;
+		
+		// Entrada
+		System.out.println("Digite o primeiro número:");
+		primeiroNumero = entrada.nextInt();
+		
+		System.out.println("Digite o segundo número:");
+		segundoNumero = entrada.nextInt();
+		
+		// Processamento
+		produto = primeiroNumero * segundoNumero;
+		
+		// Saída
+		System.out.println("O produto de " + primeiroNumero + " e " + segundoNumero + " é " + produto);
+		
+	}
+	
+}
